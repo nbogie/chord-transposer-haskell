1b4d6c99f85732bb28a54feef95d80e835eb5fb0
Neill Bogie <neillbogie@googlemail.com>
2011-03-16 17:59:51 +0000

    initial commit.  Parses reasonable set (Bbm7-9, F#M7, Cdim)
    Doesn't handle errors well (just reads as far as it can)
    
    Doesn't tackle "Am" vs "Amaj7" lookahead problem.
    For now, one could pre-process, gsubbing maj to M.  AM7 is handled.

---
+++ ChordParser.hs
@@ -0,0 +1,116 @@
+import Text.ParserCombinators.Parsec
+import Control.Monad
+import Data.Either (rights, lefts)
+
+data Note = AFlat | A | ASharp | BFlat |  B | C | CSharp | DFlat |  D | DSharp 
+                  | EFlat |  E | F | FSharp | GFlat |  G | GSharp deriving (Eq, Show, Ord)
+data ChordColor = CCMajor | CCMinor | CCDiminished| CCAugmented deriving (Eq, Show, Ord)
+data Chord = Chord {rootNote::Note, bassNote::Maybe Note, cColor::ChordColor, 
+                    cDecorations ::[String] } deriving (Eq, Show, Ord)
+
+-- This could be useful as the core of a service which could take any ascii sheet music and change key
+-- or re-write relative to the root, using roman numerals (to analyze progressions more purely).
+-- it could find and add guitar chord symbols, or make midi of the piano part, etc (but it doesn't know timing)
+-- must be able to support chords it doesn't understand (strange notations, human mistakes)
+-- We want to preserve position in the chord sheet so we can do substitutions (and extra annotations) in place
+main :: IO ()
+main    = parseAndPrint (unwords sampleInputs)
+main2    = interact parseAndShow
+
+parseAndPrint :: String -> IO ()
+parseAndPrint input = do
+  print input
+  case parse pChords "foo" input of
+    Left err -> print $ "error: " ++ (show err)
+    Right chords -> print chords
+
+parseAndShow :: String -> String
+parseAndShow input = do
+       let ls = lines input
+       let eitherChords = map (parse pChordLines "foo") ls
+       show eitherChords
+
+-- can't get this to terminate correctly - eof is always unexpected
+pChordLines :: Parser [[Chord]]
+pChordLines = do 
+  cs <- endBy pChords newline
+  eof
+  return cs
+
+sampleInputs :: [String]
+sampleInputs = [ "A"
+  , "A#7-9"
+  , "Aaug"
+  , "A9"
+  , "Bbm7"
+  , "Ab"
+  , "F#dim"
+  , "A/F#"
+  , "Am"
+  , "Am7"
+  , "AM7"
+  , "Gsus2"
+  , "Gsus4"
+  , "A#m7-5"
+  , "Am/C"
+  ]
+
+pChords :: Parser [Chord]
+pChords = sepBy pChord spaces
+
+-- can't do these yet - confusion with major seventh
+-- Amaj7
+-- Amaj7/G
+
+pChord :: Parser Chord
+-- Root [m | maj7] [dim] [7] [9] [-5 (secondary decorations)]        [/BassNote]
+pChord = do
+  r <- pNote
+  c <- pColor
+  ds <- many pDecoration <|> return []
+  bn <- pBaseNote <|> return Nothing
+  return $ Chord r bn c ds
+
+pDecoration :: Parser String
+pDecoration = do 
+  d <- string "7" <|> string "M7" <|> string "9" <|> try (string "11") <|> try (string "13") <|> try (string "sus2") <|> try (string "sus4") <|> try (string "-5") <|> string "-9" 
+  return d
+
+pBaseNote :: Parser (Maybe Note)
+pBaseNote = fmap Just (string "/" >> pNote)
+
+pNote :: Parser Note
+pNote = do
+  rRaw <- oneOf "ABCDEFG"
+  rshp <- string "#" <|> string "b" <|> return ""
+  return $ case rRaw:rshp of
+    "Ab" -> AFlat
+    "A" -> A
+    "A#" -> ASharp
+    "Bb" -> BFlat
+    "B" -> B
+    "C" -> C
+    "C#" -> CSharp
+    "Db" -> DFlat
+    "D" -> D
+    "D#" -> DSharp
+    "Eb" -> EFlat
+    "E" -> E
+    "F" -> F
+    "F#" -> FSharp
+    "Gb" -> GFlat
+    "G" -> G
+    "G#" -> GSharp
+
+-- m (not followed by an a for "maj") (peeking - don't consume)
+-- or "dim"
+-- anything else is major
+pColor :: Parser ChordColor
+pColor = do
+  s <- string "dim" <|>string "aug" <|> string "m" <|> return ""
+  return $ case s of
+            "dim" -> CCDiminished
+            "aug" -> CCAugmented
+            "m"   -> CCMinor
+            _     -> CCMajor
+
