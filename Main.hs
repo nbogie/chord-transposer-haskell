667f774baed61ab73f122953fe1eab88178c5293
Neill Bogie <neillbogie@googlemail.com>
2011-03-19 15:24:26 +0000

    Add cmd-line arg for -t (num semitones). Add cabal file.

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 import Data.Either (lefts, rights)
 import Data.List (foldl')
@@ -5,7 +6,26 @@ import Player
 import ChordParser
 import LineSplitter
 
-main = interact (printChordSheet . (transposeChordSheet 2). parseChordSheet)
+import System.Console.CmdArgs (cmdArgs, cmdArgsMode, cmdArgsRun, (&=), summary, help)
+-- provide reflection needed for cmdArgs
+import Data.Typeable
+import Data.Data
+
+main = do opts <- cmdArgsRun optsConfig
+          let t = transpose opts
+          interact (printChordSheet . (transposeChordSheet t). parseChordSheet)
+
+-- types for command-line args
+data Prog = Prog { transpose :: Int, simplify :: Bool } deriving (Data, Typeable, Show)
+
+-- set up cmd-line arg parsing, defaults, help
+optsConfig = cmdArgsMode $ Prog { 
+    transpose = 3     &= help "Set number of semitones by which to transpose the chords.",
+    simplify  = False &= help "If set, will transpose to the set of chords having fewest sharps or flats." 
+    }
+  &= summary ("Chord Sheet Transposer version "++ version)
+
+version = "0.0.1"
 
 type ChordSheetItem = (Either String Chord, (String, Int))
 
