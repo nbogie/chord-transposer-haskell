1609731d22a65e259a65a6ac0ea9d11d1cec9dd9
Neill Bogie <neillbogie@googlemail.com>
2013-03-24 22:37:11 +0000

    remove unimplemented simplify command-line switch

--- src/Main.hs
+++ src/Main.hs
@@ -16,16 +16,16 @@ main = do opts <- cmdArgsRun optsConfig
           transposeStdin fmt t asRoman
 
 -- types for command-line args
-data Prog = Prog { transpose :: Int, simplify :: Bool, html :: Bool, roman :: Bool } deriving (Data, Typeable, Show)
+data Prog = Prog { transpose :: Int, html :: Bool, roman :: Bool } deriving (Data, Typeable, Show)
 
 -- set up cmd-line arg parsing, defaults, help
 optsConfig = cmdArgsMode $ Prog 
     { transpose = 3     &= help "Set number of semitones by which to transpose the chords."
-    , simplify  = False &= help "If set, will transpose to the set of chords having fewest sharps or flats." 
+--     , simplify  = False &= help "If set, will transpose to the set of chords having fewest sharps or flats." 
     , html      = False &= help "If set, will output text as html with the chords marked up."
     , roman     = False &= help "If set, will output chords in roman numeral notation, guessing key."
     }
   &= summary ("Chord Sheet Transposer version "++ version)
 
 version :: String
-version = "0.0.3.0"
+version = "0.1.0.0"
